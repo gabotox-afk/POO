cfdd476ac8ac09511ebb5e8f342dc2d13510686f
gabotox-afk <gabrielvro1234@gmail.com>
2026-04-21 14:49:06 -0300

    ejercicio2

---
+++ Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            cronometro cronometro = new cronometro();
+            for (int i = 0; i < 5000; i++)
+            {
+                cronometro.IncrementarTiempo();
+            }
+            cronometro.MostrarTiempo();
+
+        }
+    }
+}
---
+++ Ejercicio2/Ejercicio2/cronometro.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    public class cronometro
+    {
+        private int segundos;
+        private int minutos;
+
+        public cronometro()
+        {
+            reiniciar();
+        }
+        public void reiniciar()
+        {
+            segundos = 0;
+            minutos = 0;
+        }
+
+        public void IncrementarTiempo()
+        {
+            if(segundos == 59)
+            {
+                segundos = 0;
+                minutos++;
+            }
+            else
+            {
+                segundos++;
+            }
+
+        }
+
+        public void MostrarTiempo()
+        {
+            Console.WriteLine(minutos + "minutos, " + segundos + "segundos");
+        }
+    }
+}
