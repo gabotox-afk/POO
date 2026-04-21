3af40d7f6843f05c603b10bcaa1d340170a57cc8
gabotox-afk <gabrielvro1234@gmail.com>
2026-04-21 08:09:27 -0300

    primer version

---
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Semaforo semaforo = new Semaforo("Verde");
+            semaforo.MostrarColor();
+            semaforo.PasoDelTiempo(20);
+            semaforo.ActualizarColor();
+            semaforo.MostrarColor();
+            semaforo.PasoDelTiempo(2);
+            semaforo.ActualizarColor();
+            semaforo.MostrarColor();
+            semaforo.PasoDelTiempo(30);
+            semaforo.ActualizarColor();
+            semaforo.MostrarColor();
+            semaforo.PasoDelTiempo(2);
+            semaforo.ActualizarColor();
+            semaforo.MostrarColor();
+            semaforo.PonerIntermitente();
+            semaforo.ActualizarColor();
+            semaforo.MostrarColor();
+            semaforo.PasoDelTiempo(1);
+            semaforo.MostrarColor();
+            semaforo.SacarIntermitente();
+            semaforo.ActualizarColor();
+            semaforo.MostrarColor();
+        }
+    }
+}
---
+++ Ejercicio1/Ejercicio1/Semaforo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    public class Semaforo
+    {
+        public string Color;
+        public int tiempo;
+        public bool EsIntermitente;
+        public Semaforo(string colorI)
+        { 
+            Color = colorI;
+            tiempo = 0;
+            EsIntermitente = false;
+        }
+        public void PonerIntermitente() 
+        {
+            EsIntermitente = true;
+        }
+        public void SacarIntermitente() 
+        {
+            EsIntermitente = false;
+        }
+        public void MostrarColor() 
+        {
+            if (EsIntermitente) { String estado = (tiempo % 2 == 0) ? "Amarillo" : " Apagado ";
+                Console.WriteLine("el color es: " + estado);
+            }
+            else
+            {
+                Console.WriteLine("el color es: " + Color);
+            }
+        }
+        public void PasoDelTiempo(int segundos)
+        {
+            tiempo += segundos;
+        }
+        public void ActualizarColor() 
+        {
+            if (Color == "Rojo" && tiempo >= 30) 
+            {
+                Color = "Rojo + Amarillo";
+                tiempo = 0;
+            }
+            if (Color == "Rojo + Amarillo" && tiempo >= 2)
+            {
+                Color = "Verde"; tiempo = 0;
+            }
+            if (Color == "Verde" && tiempo >= 20)
+            {
+                Color = "Amarillo"; tiempo = 0;
+            }
+            if (Color == "Amarillo" && tiempo >= 2)
+            {
+                Color = "Rojo"; tiempo = 0;
+            }
+        }
+    }
+
+}
+
